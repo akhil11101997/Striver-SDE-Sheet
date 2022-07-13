20ac4c76531bcf60c9f32986a5f9262f0d147a4f
Akhilesh Patidar <akhilesh11101997@gmail.com>
2022-07-13 09:37:42 +0530

    Create Diameter of Binary Tree.cpp

---
+++ Binary Tree/Diameter of Binary Tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int height(TreeNode* root,int &res){
+        if(root==NULL)
+            return 0;
+        int l=height(root->left,res);
+        int r=height(root->right,res);
+        res=max(res,l+r);
+        return 1+max(l,r);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int res=0;
+        height(root,res);
+        return res;
+    }
+};
